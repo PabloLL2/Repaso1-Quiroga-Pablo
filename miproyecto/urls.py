13b9a9ebf1929ce5ac754439b3295590146c43d3
Pablo Quiroga <pabloequiroga@gmail.com>
2022-03-16 20:31:57 -0300

    Cambio de Usuario

--- miproyecto/urls.py
+++ miproyecto/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('', include('index.urls')),
     path('admin/', admin.site.urls),
