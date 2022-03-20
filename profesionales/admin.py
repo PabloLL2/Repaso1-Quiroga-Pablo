f2d0819b03c26ea09d76bdec343f17da315b3a2f
Pablo Quiroga <pabloequiroga@hotmail.com>
2022-03-20 12:25:22 -0300

    After

---
+++ profesionales/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from profesionales.models import Cerrajero, Futbolista
+
+# Register your models here.
+
+admin.site.register(Cerrajero)
+admin.site.register(Futbolista)
---
+++ profesionales/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Cerrajero(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=30)
+    desempleado = models.BooleanField()
+
+
+class Futbolista(models.Model):
+    nombre = models.CharField(max_length=20)
+    apellido = models.CharField(max_length=30)
+    club_futbol = models.CharField(max_length=50 )
