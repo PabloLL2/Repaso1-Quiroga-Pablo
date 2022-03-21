643666331f666be316df6244af4b8e0e564410c6
Pablo Quiroga <pabloequiroga@hotmail.com>
2022-03-20 21:05:26 -0300

    Agregado

--- profesionales/forms.py
+++ profesionales/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class CerrajeroFormulario(forms.Form):
     nombre = forms.CharField(max_length=20)
     apellido = forms.CharField(max_length=30)
-    desempleado = forms.BooleanField()
+    desempleado = forms.BooleanField(required=False)
 
--- profesionales/views.py
+++ profesionales/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from .models import Cerrajero
 from .forms import CerrajeroFormulario
 
 # Create your views here.
 
 def crear_cerrajero(request):
+
+    if request.method == 'POST': 
+        form = CerrajeroFormulario(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            cerrajero = Cerrajero(nombre=data['nombre'], apellido=data['apellido'], desempleado=data['desempleado'])
+            cerrajero.save()
+            return render(request, "index/index.html", {})
+
     form = CerrajeroFormulario()
     return render(request, "profesionales/crear_cerrajero.html", {'form': form})
